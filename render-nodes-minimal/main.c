e57fb301f866db2a867f1d5c25ed41399f7bc140
Rob Clark <robdclark@gmail.com>
2017-04-08 08:05:32 -0400

    hack up cmdline args and read shader from file

--- render-nodes-minimal/main.c
+++ render-nodes-minimal/main.c
@@ -26,27 +26,31 @@
  * LIABILITY TO THE AUTHORS FOR ANY CLAIM OR DAMAGE.
  */
 
+#define _GNU_SOURCE
+
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 #include <GLES3/gl31.h>
 #include <assert.h>
 #include <fcntl.h>
 #include <gbm.h>
+#include <getopt.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-/* a dummy compute shader that does nothing */
-#define COMPUTE_SHADER_SRC "          \
-#version 310 es\n                                                       \
-                                                                        \
-layout (local_size_x = 1, local_size_y = 1, local_size_z = 1) in;       \
-                                                                        \
-void main(void) {                                                       \
-   /* awesome compute code here */                                      \
-}                                                                       \
-"
+static struct {
+   const char *shader;
+   const char *device;
+   struct {
+      unsigned x, y, z;
+   } num_groups;
+} opts = {
+      .device = "/dev/dri/renderD128",
+      .num_groups = { 1, 1, 1 },
+};
 
 #define get_proc(name) do {                           \
       ext.name = (void *)eglGetProcAddress(#name);    \
@@ -57,29 +61,40 @@ static struct {
 } ext;
 
 
-int32_t
-main (int32_t argc, char* argv[])
+const char *readfile(int fd)
+{
+   static char text[64 * 1024];
+   int ret = read(fd, text, sizeof(text));
+   if (ret < 0) {
+      printf("error reading shader: %d\n", ret);
+      exit(-1);
+   }
+   text[ret] = '\0';
+   return strdup(text);
+}
+
+static void run(void)
 {
    bool res;
 
-   int32_t fd = open ("/dev/dri/renderD128", O_RDWR);
-   assert (fd > 0);
+   int32_t fd = open(opts.device, O_RDWR);
+   assert(fd > 0);
 
-   struct gbm_device *gbm = gbm_create_device (fd);
-   assert (gbm != NULL);
+   struct gbm_device *gbm = gbm_create_device(fd);
+   assert(gbm != NULL);
 
    get_proc(glDispatchCompute);
 
    /* setup EGL from the GBM device */
-   EGLDisplay egl_dpy = eglGetPlatformDisplay (EGL_PLATFORM_GBM_MESA, gbm, NULL);
-   assert (egl_dpy != NULL);
+   EGLDisplay egl_dpy = eglGetPlatformDisplay(EGL_PLATFORM_GBM_MESA, gbm, NULL);
+   assert(egl_dpy != NULL);
 
-   res = eglInitialize (egl_dpy, NULL, NULL);
-   assert (res);
+   res = eglInitialize(egl_dpy, NULL, NULL);
+   assert(res);
 
-   const char *egl_extension_st = eglQueryString (egl_dpy, EGL_EXTENSIONS);
-   assert (strstr (egl_extension_st, "EGL_KHR_create_context") != NULL);
-   assert (strstr (egl_extension_st, "EGL_KHR_surfaceless_context") != NULL);
+   const char *egl_extension_st = eglQueryString(egl_dpy, EGL_EXTENSIONS);
+   assert(strstr(egl_extension_st, "EGL_KHR_create_context") != NULL);
+   assert(strstr(egl_extension_st, "EGL_KHR_surfaceless_context") != NULL);
 
    static const EGLint config_attribs[] = {
       EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT_KHR,
@@ -88,89 +103,137 @@ main (int32_t argc, char* argv[])
    EGLConfig cfg;
    EGLint count;
 
-   res = eglChooseConfig (egl_dpy, config_attribs, &cfg, 1, &count);
-   assert (res);
+   res = eglChooseConfig(egl_dpy, config_attribs, &cfg, 1, &count);
+   assert(res);
 
-   res = eglBindAPI (EGL_OPENGL_ES_API);
-   assert (res);
+   res = eglBindAPI(EGL_OPENGL_ES_API);
+   assert(res);
 
    static const EGLint attribs[] = {
       EGL_CONTEXT_CLIENT_VERSION, 3,
       EGL_NONE
    };
-   EGLContext core_ctx = eglCreateContext (egl_dpy,
-                                           cfg,
-                                           EGL_NO_CONTEXT,
-                                           attribs);
-   assert (core_ctx != EGL_NO_CONTEXT);
+   EGLContext core_ctx =
+         eglCreateContext(egl_dpy, cfg, EGL_NO_CONTEXT, attribs);
+   assert(core_ctx != EGL_NO_CONTEXT);
 
-   res = eglMakeCurrent (egl_dpy, EGL_NO_SURFACE, EGL_NO_SURFACE, core_ctx);
-   assert (res);
+   res = eglMakeCurrent(egl_dpy, EGL_NO_SURFACE, EGL_NO_SURFACE, core_ctx);
+   assert(res);
 
    /* print some compute limits (not strictly necessary) */
    GLint work_group_count[3] = {0};
    for (unsigned i = 0; i < 3; i++)
-      glGetIntegeri_v (GL_MAX_COMPUTE_WORK_GROUP_COUNT,
+      glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT,
                        i,
                        &work_group_count[i]);
-   printf ("GL_MAX_COMPUTE_WORK_GROUP_COUNT: %d, %d, %d\n",
+   printf("GL_MAX_COMPUTE_WORK_GROUP_COUNT: %d, %d, %d\n",
            work_group_count[0],
            work_group_count[1],
            work_group_count[2]);
 
    GLint work_group_size[3] = {0};
    for (unsigned i = 0; i < 3; i++)
-      glGetIntegeri_v (GL_MAX_COMPUTE_WORK_GROUP_SIZE, i, &work_group_size[i]);
-   printf ("GL_MAX_COMPUTE_WORK_GROUP_SIZE: %d, %d, %d\n",
+      glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, i, &work_group_size[i]);
+   printf("GL_MAX_COMPUTE_WORK_GROUP_SIZE: %d, %d, %d\n",
            work_group_size[0],
            work_group_size[1],
            work_group_size[2]);
 
    GLint max_invocations;
-   glGetIntegerv (GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS, &max_invocations);
-   printf ("GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS: %d\n", max_invocations);
+   glGetIntegerv(GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS, &max_invocations);
+   printf("GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS: %d\n", max_invocations);
 
    GLint mem_size;
-   glGetIntegerv (GL_MAX_COMPUTE_SHARED_MEMORY_SIZE, &mem_size);
-   printf ("GL_MAX_COMPUTE_SHARED_MEMORY_SIZE: %d\n", mem_size);
+   glGetIntegerv(GL_MAX_COMPUTE_SHARED_MEMORY_SIZE, &mem_size);
+   printf("GL_MAX_COMPUTE_SHARED_MEMORY_SIZE: %d\n", mem_size);
 
    /* setup a compute shader */
-   GLuint compute_shader = glCreateShader (GL_COMPUTE_SHADER);
+   GLuint compute_shader = glCreateShader(GL_COMPUTE_SHADER);
 
-   assert (glGetError () == GL_NO_ERROR);
-   const char *shader_source = COMPUTE_SHADER_SRC;
+   assert(glGetError() == GL_NO_ERROR);
 
-   glShaderSource (compute_shader, 1, &shader_source, NULL);
-   assert (glGetError () == GL_NO_ERROR);
+   glShaderSource(compute_shader, 1, &opts.shader, NULL);
+   assert(glGetError() == GL_NO_ERROR);
 
-   glCompileShader (compute_shader);
-   assert (glGetError () == GL_NO_ERROR);
+   glCompileShader(compute_shader);
+   assert(glGetError() == GL_NO_ERROR);
 
-   GLuint shader_program = glCreateProgram ();
+   GLuint shader_program = glCreateProgram();
 
-   glAttachShader (shader_program, compute_shader);
-   assert (glGetError () == GL_NO_ERROR);
+   glAttachShader(shader_program, compute_shader);
+   assert(glGetError() == GL_NO_ERROR);
 
-   glLinkProgram (shader_program);
-   assert (glGetError () == GL_NO_ERROR);
+   glLinkProgram(shader_program);
+   assert(glGetError() == GL_NO_ERROR);
 
-   glDeleteShader (compute_shader);
+   glDeleteShader(compute_shader);
 
-   glUseProgram (shader_program);
-   assert (glGetError () == GL_NO_ERROR);
+   glUseProgram(shader_program);
+   assert(glGetError() == GL_NO_ERROR);
 
    /* dispatch computation */
-   ext.glDispatchCompute (1, 1, 1);
-   assert (glGetError () == GL_NO_ERROR);
+   ext.glDispatchCompute(opts.num_groups.x, opts.num_groups.y, opts.num_groups.z);
+   assert(glGetError() == GL_NO_ERROR);
 
-   printf ("Compute shader dispatched and finished successfully\n");
+   printf("Compute shader dispatched and finished successfully\n");
 
    /* free stuff */
-   glDeleteProgram (shader_program);
-   eglDestroyContext (egl_dpy, core_ctx);
-   eglTerminate (egl_dpy);
-   gbm_device_destroy (gbm);
-   close (fd);
+   glDeleteProgram(shader_program);
+   eglDestroyContext(egl_dpy, core_ctx);
+   eglTerminate(egl_dpy);
+   gbm_device_destroy(gbm);
+   close(fd);
+}
+
+static const char *shortopts = "D:G:";
+
+static const struct option longopts[] = {
+      {"device", required_argument, 0, 'D'},
+      {"groups", required_argument, 0, 'G'},
+      {0, 0, 0, 0}
+};
+
+static void usage(const char *name)
+{
+   printf("Usage: %s [-DG] SHADER\n"
+         "\n"
+         "options:\n"
+         "    -D, --device=DEVICE      use the given device\n"
+         "    -G, --groups=X,Y,Z       use specified group size\n"
+         ,
+         name);
+}
+
+int main(int argc, char *argv[])
+{
+   int opt, ret;
+
+   while ((opt = getopt_long_only(argc, argv, shortopts, longopts, NULL)) != -1) {
+      switch (opt) {
+      case 'D':
+         opts.device = optarg;
+         break;
+      case 'G':
+         ret = sscanf(optarg, "%u,%u,%u", &opts.num_groups.x,
+               &opts.num_groups.y, &opts.num_groups.z);
+         if (ret != 3)
+            goto usage;
+         break;
+      default:
+         goto usage;
+      }
+   }
+
+   if (optind != (argc - 1))
+      goto usage;
+
+   opts.shader = readfile(open(argv[optind], 0));
+
+   run();
 
    return 0;
+
+usage:
+   usage(argv[0]);
+   return -1;
 }
